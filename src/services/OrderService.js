552075a56c6cdffae807d897d64b98053f394fc6
1cuhp <hoangphucagg@gmail.com>
2025-02-13 22:07:15 +0700

    View-order-BE

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -3,7 +3,8 @@ const FuelRequest = require("../models/Fuel_Request");
 
 const getAllorderbySucess = async () => {
   try {
-    const approvedOrders = await FuelRequest.find({ status: "Đã duyệt", is_deleted: false });
+    const approvedOrders = await FuelRequest.find({ status: "Đã duyệt", is_deleted: false }).populate('supplier_id');
+    console.log(approvedOrders)
     return {
       success: true,
       data: approvedOrders,
