fe81fd70c0a3964fd36747479e8d76b25d5bb87e
unknown <hbao170403@gmail.com>
2025-02-27 14:52:34 +0700

    thay doi nhe

--- src/services/FuelService.js
+++ src/services/FuelService.js
@@ -25,54 +25,54 @@ const getAllFuel = async (filters) => {
   }
 };
 
-const updateFuel = async (id, data) => {
-  try {
-    const updatedFuel = await FuelTypes.findByIdAndUpdate(
-      id,
-      { 
-        type_name: data.type_name,
-        description: data.description,
-        updatedAt: new Date(),
-      },
-      { new: true }
-    );
+  const updateFuel = async (id, data) => {
+    try {
+      const updatedFuel = await FuelTypes.findByIdAndUpdate(
+        id,
+        { 
+          type_name: data.type_name,
+          description: data.description,
+          updatedAt: new Date(),
+        },
+        { new: true }
+      );
 
-    if (!updatedFuel) {
-      throw new Error("Không tìm thấy nhiên liệu!");
+      if (!updatedFuel) {
+        throw new Error("Không tìm thấy nhiên liệu!");
+      }
+
+      return {
+        success: true,
+        message: "Cập nhật thành công!",
+        data: updatedFuel,
+      };
+    } catch (error) {
+      throw new Error(error.message);
     }
+  };
 
-    return {
-      success: true,
-      message: "Cập nhật thành công!",
-      data: updatedFuel,
-    };
-  } catch (error) {
-    throw new Error(error.message);
-  }
-};
+  // ✅ Đổi deleteFuel -> cancelFuel (Chỉ đánh dấu là 'Đã xóa', không xóa khỏi DB)
+  const cancelFuel = async (id) => {
+    try {
+      const canceledFuel = await FuelTypes.findByIdAndUpdate(
+        id,
+        { is_deleted: true, updatedAt: new Date() },
+        { new: true }
+      );
 
-// ✅ Đổi deleteFuel -> cancelFuel (Chỉ đánh dấu là 'Đã xóa', không xóa khỏi DB)
-const cancelFuel = async (id) => {
-  try {
-    const canceledFuel = await FuelTypes.findByIdAndUpdate(
-      id,
-      { is_deleted: true, updatedAt: new Date() },
-      { new: true }
-    );
+      if (!canceledFuel) {
+        throw new Error("Không tìm thấy nhiên liệu!");
+      }
 
-    if (!canceledFuel) {
-      throw new Error("Không tìm thấy nhiên liệu!");
+      return {
+        success: true,
+        message: "Đã đánh dấu nhiên liệu là 'Đã xóa'!",
+        data: canceledFuel,
+      };
+    } catch (error) {
+      throw new Error(error.message);
     }
-
-    return {
-      success: true,
-      message: "Đã đánh dấu nhiên liệu là 'Đã xóa'!",
-      data: canceledFuel,
-    };
-  } catch (error) {
-    throw new Error(error.message);
-  }
-};
+  };
 
 module.exports = { getAllFuel,
    updateFuel, 
