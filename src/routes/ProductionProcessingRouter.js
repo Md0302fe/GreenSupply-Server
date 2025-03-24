cbe3bda7926d3ef0a348dbfdab7b7446eb6dbd0e
Md0302fe <minhduc2003kols@gmail.com>
2025-03-24 20:07:10 +0700

    production process

--- src/routes/ProductionProcessingRouter.js
+++ src/routes/ProductionProcessingRouter.js
@@ -13,9 +13,12 @@ const {
   // Routes cho Address
   router.post("/create", ProductionProcessingController.create);
   router.get("/getAll", ProductionProcessingController.getAll);
+  router.get("/getAllExecuteProcess", ProductionProcessingController.getAllExecuteProcess);
   router.get("/getAllProcessing", ProductionProcessingController.getAllProcessing);
   router.put("/:id", ProductionProcessingController.update);
   router.delete("/:id", ProductionProcessingController.deleteById);
   router.put("/change-status/:id", ProductionProcessingController.changeStatus);
-
+  router.get("/details/:id",ProductionProcessingController.getDetailsProcess);
+  router.get("/detailsStage/:id",ProductionProcessingController.getProcessStage);
+  router.post("/finishStage/", ProductionProcessingController.finishStage);
   module.exports = router;
